a9c1b85538bf966248e028b998a1bd27b7331f60
EricBernardes <ericbernardes70@gmail.com>
2023-09-28 01:06:07 -0300

    Aluno-java

---
+++ Aluno.java
@@ -0,0 +1,12 @@
+public class Aluno {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String novoNome) {
+        nome = novoNome;
+    }
+
+}
---
+++ TestaAluno.java
@@ -0,0 +1,7 @@
+public class TestaAluno {
+    public static void main(String[] args) {
+        Aluno a = new Aluno();
+        a.setNome("Eric");
+        System.out.println(a.getNome());
+    }
+}
